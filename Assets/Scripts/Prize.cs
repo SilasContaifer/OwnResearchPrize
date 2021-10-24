94cc0e2dcdac2760f7de5cd5e6ba956ff9598adc
Silas Contaifer <42838875+SilasContaifer@users.noreply.github.com>
2021-10-24 03:05:12 -0400

    Covid-19 and Prize Information
    Scenes showing materials as videos, blog posts and Scientific papers about Covid-19 separated by different user segmentation.

---
+++ Assets/Scripts/Prize.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Prize : MonoBehaviour
+{
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("Prize", LoadSceneMode.Single);//We will load the scene named in the inspector it will be .Single because it will close the previous scene
+    }
+}
